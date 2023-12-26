38520872066bd3b3db533b0d01906fae760e1d23
Engin Gülek <engingulek0@gmail.com>
2023-12-26 21:43:24 +0300

    Create Workspace and DependencyKit

---
+++ DependenyKit/DependenyKit/DependenyKit.h
@@ -0,0 +1,18 @@
+//
+//  DependenyKit.h
+//  DependenyKit
+//
+//  Created by engin gülek on 26.12.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DependenyKit.
+FOUNDATION_EXPORT double DependenyKitVersionNumber;
+
+//! Project version string for DependenyKit.
+FOUNDATION_EXPORT const unsigned char DependenyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DependenyKit/PublicHeader.h>
+
+
---
+++ MovieTicket/MovieTicket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTicket
+//
+//  Created by engin gülek on 26.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

94fe99b6a0adc9212681a40d0577476e42cafde2
Engin Gülek <engingulek0@gmail.com>
2023-12-28 15:26:51 +0300

    Create Group for HomeModule

---
+++ MovieDetail/MovieDetail/MovieDetail.h
@@ -0,0 +1,18 @@
+//
+//  MovieDetail.h
+//  MovieDetail
+//
+//  Created by engin gülek on 28.12.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovieDetail.
+FOUNDATION_EXPORT double MovieDetailVersionNumber;
+
+//! Project version string for MovieDetail.
+FOUNDATION_EXPORT const unsigned char MovieDetailVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovieDetail/PublicHeader.h>
+
+

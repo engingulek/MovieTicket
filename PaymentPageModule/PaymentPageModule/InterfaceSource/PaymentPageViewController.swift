fd9d392b4806c175d4a06f5c3ecddca252b25fed
Engin Gülek <engingulek0@gmail.com>
2024-01-03 12:54:00 +0300

    Created PaymentPageModule and PaymetPageApp

---
+++ PaymentPageModule/PaymentPageModule/InterfaceSource/PaymentPageViewController.swift
@@ -0,0 +1,14 @@
+import Foundation
+import UIKit
+
+
+protocol PaymentPageViewControllerInterface : AnyObject {
+    
+}
+
+
+final class PaymentPageViewController : UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor =  .red
+    }
+}
---
+++ PaymentPageModule/PaymentPageModule/PaymentPageModule.h
@@ -0,0 +1,18 @@
+//
+//  PaymentPageModule.h
+//  PaymentPageModule
+//
+//  Created by engin gülek on 3.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PaymentPageModule.
+FOUNDATION_EXPORT double PaymentPageModuleVersionNumber;
+
+//! Project version string for PaymentPageModule.
+FOUNDATION_EXPORT const unsigned char PaymentPageModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaymentPageModule/PublicHeader.h>
+
+
